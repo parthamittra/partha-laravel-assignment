f92db1fcfd23810cd23aca9f0b8aa6b66ab369f9
Partha Mittra <parthamittra@system76-pc.localdomain>
2022-10-23 05:15:46 -0400

    fixed merge issue

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+           "username"=>"User_with_bank_account",
+           "income" => 187600,
+            "employment_flag" => 1
+        ]);
+
+        DB::table('users')->insert([
+            "username"=>"User_without_bank_account",
+            "income" => 140000,
+            "employment_flag" => 1
+        ]);
+
+    }
+}
